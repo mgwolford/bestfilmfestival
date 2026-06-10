63438b8adf6e2633b40ddcdf23353513dc9ee0a9
Matthew Wolford <mg.wolford@outlook.com>
2026-06-10 14:21:02 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./App.css";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/bestfilmfestival/",
-});
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
